cc5a1ad654779491dbd833f95ef43f5ce7a47afc
Larissa Moreira <larimlisboa@gmail.com>
2024-02-29 16:19:31 -0300

    Fix bug on locale

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { HistoryContainer, HistoryList, Status } from "./styles";
-import ptBR from "date-fns/locale/pt-BR";
+import {ptBR} from "date-fns/locale/pt-BR";
 import { CyclesContext } from "../../contexts/CyclesContext";
 import { formatDistanceToNow } from "date-fns";
 export function History() {
